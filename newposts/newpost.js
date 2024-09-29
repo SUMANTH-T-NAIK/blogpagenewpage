a90b08c37105dbb63832a1a00c14881d1ecfe5da
SUMANTH-T-NAIK <sumanthnaik523@gmail.com>
2024-09-29 19:24:20 +0530

    Delete newposts directory

--- newposts/newpost.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('update-button').addEventListener('click', function() {
-    const heading = document.getElementById('new-posts-heading');
-    heading.textContent = `New Posts (${Math.floor(Math.random() * 100) + 1})`;
-});
